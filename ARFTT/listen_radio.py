aa054e8a179754e8df2d4066cad2021985bdfdbd
William Joshua King <wgk4@duke.edu>
2024-02-22 15:21:01 -0500

    Added squelch -- Minimized static

--- ARFTT/listen_radio.py
+++ ARFTT/listen_radio.py
@@ -4,6 +4,7 @@ import argparse
 import numpy as np
 import pyaudio
 import scipy.signal as signal
+import matplotlib.pyplot as plt
 
 SampleStream = List[float]
 AudioStream = List[int]
@@ -14,20 +15,21 @@ stream_counter = 0
 audio_rate = 48000
 audio_output = pyaudio.PyAudio().open(format=pyaudio.paInt16, channels=1, rate=audio_rate, output=True)
 
+
 def process(samples: SampleStream, sdr: RtlSdr) -> None:
     sample_rate_fm = 240000
-    iq_comercial = signal.decimate(samples, int(sdr.get_sample_rate()) // sample_rate_fm)
+    iq_commercial = signal.decimate(samples, int(sdr.get_sample_rate()) // sample_rate_fm)
 
-    angle_comercial = np.unwrap(np.angle(iq_comercial))
-    demodulated_comercial = np.diff(angle_comercial)
+    angle_commercial = np.unwrap(np.angle(iq_commercial))
+    demodulated_commercial = np.diff(angle_commercial)
 
-    audio_signal = signal.decimate(demodulated_comercial, sample_rate_fm // audio_rate, zero_phase=True)
+    audio_signal = signal.decimate(demodulated_commercial, sample_rate_fm // audio_rate, zero_phase=True)
     audio_signal = np.int16(14000 * audio_signal)
 
     # Apply squelch to the demodulated audio
     squelched_audio = apply_squelch(audio_signal, squelch)
 
-    audio_output.write(audio_signal.astype("int16").tobytes())
+    audio_output.write(squelched_audio.astype("int16").tobytes())
 
 
 def apply_squelch(audio_signal: np.ndarray, squelch_threshold) -> np.ndarray:
@@ -50,11 +52,11 @@ def read_callback(samples, rtl_sdr_obj):
 parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 parser.add_argument('--ppm', type=int, default=0,
                     help='ppm error correction')
-parser.add_argument('--gain', type=int, default="auto",
+parser.add_argument('--gain', type=int, default=20,
                     help='RF gain level')
-parser.add_argument('--freq', type=int, default=92900000,
+parser.add_argument('--freq', type=int, default=147410000,
                     help='frequency to listen to, in Hertz')
-parser.add_argument('--squelch', type=float, default=0.01,
+parser.add_argument('--squelch', type=float, default=1000,
                     help='minimum threshold required for audio output to show')
 parser.add_argument('--verbose', action='store_true',
                     help='mute audio output')
@@ -64,7 +66,7 @@ args = parser.parse_args()
 sdr = RtlSdr()
 sdr.rs = 1024000
 sdr.fc = args.freq
-sdr.gain = args.gain
+sdr.gain = "auto"
 sdr.err_ppm = args.ppm
 squelch = args.squelch
 
